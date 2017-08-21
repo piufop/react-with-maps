934a44761ec0534255f6c878e8f8e44c8752c0bc
piufop <piufop@gmail.com>
2017-08-21 15:56:43 -0300

    Fetch points

--- src/maps/java-script.js
+++ src/maps/java-script.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 import TopBar from '../topbar/topbar';
 import BottomBar from '../bottombar/bottombar';
 import GoogleMapReact from 'google-map-react';
@@ -13,18 +14,24 @@ const style = {
   height: '500px'
 };
 
-let points = [];
-for (let i = 0; i < 200; i++) {
-  points.push({
-    latitude: chance.latitude({ min: -19.99, max: -19.77 }),
-    longitude: chance.longitude({ min: -44.10, max: -43.85 }),
-    weight: chance.integer({ min: 1, max: 50 })
-  })
-}
-
 export default class JavaScriptMap extends React.Component {
   constructor(props) {
     super();
+    this.state = {
+      points: []
+    };
+  }
+
+  getPoints() {
+    let points = [];
+    for (let i = 0; i < 500; i++) {
+      points.push({
+        latitude: chance.latitude({ min: -19.99, max: -19.77 }),
+        longitude: chance.longitude({ min: -44.10, max: -43.85 }),
+        weight: chance.integer({ min: 1, max: 50 })
+      })
+    }
+    this.setState({ points });
   }
 
   render() {
@@ -32,6 +39,7 @@ export default class JavaScriptMap extends React.Component {
       <div>
         <TopBar title={'JavaScript'} />
         <div style={style}>
+          <RaisedButton label='Get points' onClick={this.getPoints.bind(this)} />
           <GoogleMapReact bootstrapURLKeys={{
             key: config.GOOGLE_API_KEY,
             libraries: 'visualization'
@@ -39,13 +47,8 @@ export default class JavaScriptMap extends React.Component {
             center={this.props.center} zoom={this.props.zoom}
             yesIWantToUseGoogleMapApiInternals
             onGoogleApiLoaded={({ map, maps }) => {
-
               const heatmap = new maps.visualization.HeatmapLayer({
-                data: points.map(point => (
-                  {
-                    location: new maps.LatLng(point.latitude, point.longitude),
-                    weight: point.weight
-                  }))
+                data: this.state.points
               });
               heatmap.setMap(map);
             }}>
---
+++ src/populate.js
@@ -0,0 +1,37 @@
+const firebase = require('firebase');
+const config = require('./config/config');
+const Chance = require('chance');
+
+const chance = new Chance();
+
+const firebaseConfig = {
+  apiKey: config.FIREBASE_API_KEY,
+  authDomain: config.FIREBASE_AUTH_DOMAIN,
+  databaseURL: config.FIREBASE_DATABASE_URL,
+  projectId: config.FIREBASE_PROJECT_ID,
+  storageBucket: config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+let points = [];
+
+firebase.auth().signInWithEmailAndPassword(config.FIREBASE_EMAIL, config.FIREBASE_PASSWORD).then(() => {
+  console.log('User authenticated');
+  for (let i = 0; i < 500; i++) {
+    database.ref('points/' + i).set({
+      latitude: chance.latitude({ min: -19.99, max: -19.77 }),
+      longitude: chance.longitude({ min: -44.10, max: -43.85 })
+    }).then((result) => {
+      console.log('Result:', result);
+    }).catch((error) => {
+      console.log('Error:', error);
+    });
+  }
+}).catch((authError) => {
+  console.log(`Error code ${errorAuth.code}. Message: ${errorAuth.message}`);
+});
+
+
