2107b4beb4eaa48a7bde9b31a7345b6928457b80
piufop <piufop@gmail.com>
2017-08-22 17:03:51 -0300

    Update to use fetch points

--- src/maps/java-script.js
+++ src/maps/java-script.js
@@ -2,12 +2,11 @@ import React from 'react';
 import BottomBar from '../bottombar/bottombar';
 import GoogleMapReact from 'google-map-react';
 import config from '../config/config';
-import Chance from 'chance';
 import IconButton from 'material-ui/IconButton';
 import NavigationRefresh from 'material-ui/svg-icons/navigation/refresh';
 import NavigationCancel from 'material-ui/svg-icons/navigation/cancel';
 
-const chance = new Chance();
+import { getPoints } from './points';
 
 const style = {
   display: 'inline-block',
@@ -20,17 +19,19 @@ export default class JavaScriptMap extends React.Component {
     super();
   }
 
-  getPoints() {
-    let points = [];
-    for (let i = 0; i < 500; i++) {
-      points.push({
-        location: new this.maps.LatLng(chance.latitude({ min: -20.10, max: -19.70 }), chance.longitude({ min: -44.30, max: -43.70 }))
-      })
-    }
-    this.heatmap.setData(points);
+  fetch() {
+    getPoints((result) => {
+      let points = [];
+      result.map((point) => {
+        points.push({
+          location: new this.maps.LatLng(point.latitude, point.longitude)
+        });
+      });
+      this.heatmap.setData(points);
+    });
   }
 
-  clearPoints() {
+  clear() {
     this.heatmap.setData([]);
   }
 
@@ -39,8 +40,8 @@ export default class JavaScriptMap extends React.Component {
       <div>
         <div style={style}>
           <div >
-            <IconButton onClick={this.getPoints.bind(this)}><NavigationRefresh /></IconButton>
-            <IconButton onClick={this.clearPoints.bind(this)}><NavigationCancel /></IconButton>
+            <IconButton onClick={this.fetch.bind(this)}><NavigationRefresh /></IconButton>
+            <IconButton onClick={this.clear.bind(this)}><NavigationCancel /></IconButton>
           </div>
           <GoogleMapReact bootstrapURLKeys={{
             key: config.GOOGLE_API_KEY,
