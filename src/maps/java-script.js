d3f781d5330b673348e0b16b8c7877bf46028d80
piufop <piufop@gmail.com>
2017-08-22 14:47:21 -0300

    Add buttons to get points and clear the map

--- src/maps/java-script.js
+++ src/maps/java-script.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import TopBar from '../topbar/topbar';
 import BottomBar from '../bottombar/bottombar';
 import GoogleMapReact from 'google-map-react';
 import config from '../config/config';
 import Chance from 'chance';
+import IconButton from 'material-ui/IconButton';
+import NavigationRefresh from 'material-ui/svg-icons/navigation/refresh';
+import NavigationCancel from 'material-ui/svg-icons/navigation/cancel';
 
 const chance = new Chance();
 
@@ -17,27 +18,30 @@ const style = {
 export default class JavaScriptMap extends React.Component {
   constructor(props) {
     super();
-    this.state = {
-      points: []
-    };
   }
 
   getPoints() {
     let points = [];
     for (let i = 0; i < 500; i++) {
       points.push({
-        location: new this.maps.LatLng(chance.latitude({ min: -19.99, max: -19.77 }), chance.longitude({ min: -44.10, max: -43.85 }))
+        location: new this.maps.LatLng(chance.latitude({ min: -20.10, max: -19.70 }), chance.longitude({ min: -44.30, max: -43.70 }))
       })
     }
     this.heatmap.setData(points);
   }
 
+  clearPoints() {
+    this.heatmap.setData([]);
+  }
+
   render() {
     return (
       <div>
-        <TopBar title={'JavaScript'} />
         <div style={style}>
-          <RaisedButton label='Get points' onClick={this.getPoints.bind(this)} />
+          <div >
+            <IconButton onClick={this.getPoints.bind(this)}><NavigationRefresh /></IconButton>
+            <IconButton onClick={this.clearPoints.bind(this)}><NavigationCancel /></IconButton>
+          </div>
           <GoogleMapReact bootstrapURLKeys={{
             key: config.GOOGLE_API_KEY,
             libraries: 'visualization'
@@ -50,7 +54,6 @@ export default class JavaScriptMap extends React.Component {
               this.heatmap = new this.maps.visualization.HeatmapLayer();
               this.heatmap.setMap(map);
             }}>
-
           </GoogleMapReact>
         </div>
         <BottomBar />
