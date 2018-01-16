96e71ffcd7f7a4a6a6088a85ba65480801055ee3
Hugo Machado <piufop@gmail.com>
2018-01-16 15:25:58 -0200

    Fixes test script

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
